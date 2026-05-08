9defc95577a7d8937c343187a704271ce2845418
YasuGoto <yasuko.goto@nijibox.co.jp>
2026-05-08 23:59:33 +0900

    API関数作成

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,12 +1,32 @@
 "use client";
 import { useState, useEffect } from "react";
+import { getActiveStudySession } from "@/lib/study-session/fetch";
+import { getDailySummary } from "@/lib/daily-summary/fetch";
 
 export default function Home() {
   const [isActive, setIsActive] = useState(false);
   const [totalTime, setTotalTime] = useState(0);
 
   useEffect(() => {
-    if (isActive) {
-    }
-  }, [isActive]);
+    const fetchActiveStudySession = async () => {
+      const response = await getActiveStudySession();
+      if (!response) {
+        setIsActive(false);
+        return;
+      }
+      if (response.stoppedDate) {
+        setIsActive(false);
+      } else {
+        setIsActive(true);
+      }
+    };
+    const fetchTotalTime = async () => {
+      const response = await getDailySummary(
+        new Date().toISOString().split("T")[0],
+      );
+      setTotalTime(response.totalSeconds ?? 0);
+    };
+    fetchActiveStudySession();
+    fetchTotalTime();
+  }, []);
 }
