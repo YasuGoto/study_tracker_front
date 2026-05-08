b3ae28d6107c24c0dc7336ce48673e848f6903e8
YasuGoto <yasuko.goto@nijibox.co.jp>
2026-05-09 01:27:20 +0900

    ホーム画面実装・タイムゾーン対応

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,11 +1,47 @@
 "use client";
 import { useState, useEffect } from "react";
-import { getActiveStudySession } from "@/lib/study-session/fetch";
-import { getDailySummary } from "@/lib/daily-summary/fetch";
+import {
+  getActiveStudySession,
+  startStudySession,
+  stopStudySession,
+} from "@/lib/study-session/fetch";
+import { getDailySummary, createDailySummary } from "@/lib/daily-summary/fetch";
 
 export default function Home() {
   const [isActive, setIsActive] = useState(false);
   const [totalTime, setTotalTime] = useState(0);
+  const fetchDailySummary = async () => {
+    const response = await createDailySummary();
+    if (!response) {
+      return;
+    }
+    return response;
+  };
+  const fetchTotalTime = async () => {
+    try {
+      const jstNow = new Date(new Date().getTime() + 9 * 60 * 60 * 1000);
+      const jstDate = jstNow.toISOString().split("T")[0];
+      const response = await getDailySummary(jstDate);
+      if (!response) return;
+      setTotalTime(response.totalSeconds ?? 0);
+    } catch (error) {}
+  };
+  const handleStart = async () => {
+    const response = await startStudySession();
+    if (!response) {
+      return;
+    }
+    setIsActive(true);
+  };
+  const handleStop = async () => {
+    const response = await stopStudySession();
+    if (!response) {
+      return;
+    }
+    await fetchDailySummary();
+    await fetchTotalTime();
+    setIsActive(false);
+  };
 
   useEffect(() => {
     const fetchActiveStudySession = async () => {
@@ -20,13 +56,19 @@ export default function Home() {
         setIsActive(true);
       }
     };
-    const fetchTotalTime = async () => {
-      const response = await getDailySummary(
-        new Date().toISOString().split("T")[0],
-      );
-      setTotalTime(response.totalSeconds ?? 0);
-    };
     fetchActiveStudySession();
     fetchTotalTime();
   }, []);
+
+  return (
+    <div>
+      <h1>Home</h1>
+      {isActive ? (
+        <button onClick={handleStop}>Stop</button>
+      ) : (
+        <button onClick={handleStart}>Start</button>
+      )}
+      <p>Total Time: {totalTime}</p>
+    </div>
+  );
 }
